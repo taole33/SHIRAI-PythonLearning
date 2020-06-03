f0bae012444bf876832fc28f02852dfc6491ecf6
Shirai <taole33@hotmail.com>
2020-06-03 21:20:49 +0900

    Update 004stockratio.py

--- 004stockratio.py
+++ 004stockratio.py
@@ -1,5 +1,11 @@
 import tkinter
 from tkinter import messagebox
+import requests
+from bs4 import BeautifulSoup as BS
+import pandas
+import urllib.parse
+import numpy
+
 
 tki = tkinter.Tk()
 tki.geometry('500x400') 
@@ -7,18 +13,11 @@ tki.title('調べたい株価のコード入力')
 
 
 def soukankeisu():
-    import requests
-    from bs4 import BeautifulSoup as BS
-    import pandas
-    import urllib.parse
-    import numpy
-    from tkinter import messagebox
-    
-    shinyoubairitsu=[]
-    kabuka=[]
+
+    margin_ratio=[]
+    stock_price=[]
     code=txt.get()
 
-    print(type(code))
     #if not type(code) == 'int':
     # messagebox.showinfo('エラー','数字４桁を入力してください')
     #else:
@@ -27,28 +26,44 @@ def soukankeisu():
     #  messagebox.showinfo('エラー','数字４桁を入力してください')
 
     #else:
- 
-    html = requests.get('https://kabutan.jp/stock/kabuka?code='+code+'&ashi=shin')
-    kaiseki = BS(html.content,'html.parser')
-
-    table = kaiseki.find('table',class_='stock_kabuka3')
-    rows = table.find_all("tr")
-
-    for i in range(28):
-        hiritsurow  = rows[2+i]
-        column = hiritsurow.find_all("td")
-        owarine = column[2].text
-        owarinefloat=float(owarine.replace(',',''))
-        kabuka.append(owarinefloat)
-        hiritsu = column[6].text
-        hiritsufloat= float(hiritsu.replace(',',''))
-        shinyoubairitsu.append(hiritsufloat)
-
-    a3 = numpy.corrcoef(kabuka,shinyoubairitsu)[0]
-
-
+    # <div class="si_i1_1">
+    #<h2><span>4661</span>オリエンタルランド</h2>
     
-    messagebox.showinfo('結果','相関係数は'+str(a3)+'です')
+
+    html = requests.get('https://kabutan.jp/stock/kabuka?code='+code+'&ashi=shin')
+    if not html.status_code == 200:
+        messagebox.showinfo('エラー','webページに問題が発生しました')
+    else:
+        analyse = BS(html.content,'html.parser')
+        stock_name = analyse.find('div',class_='si_i1_1')
+
+        if not stock_name:
+            messagebox.showinfo('エラー','銘柄が見つかりませんでした')
+        else:
+            
+            table = analyse.find('table',class_='stock_kabuka3')
+            rows = table.find_all("tr")
+
+            for i in range(28):
+                ratiorow  = rows[2+i]
+                column = ratiorow.find_all("td")
+                price = column[2].text
+                pricefloat=float(price.replace(',',''))
+                stock_price.append(pricefloat)
+                ratio = column[6].text
+                if ratio == "－":
+                    messagebox.showinfo('エラー','信用倍率が存在しないため計算できません')
+                    break
+                else:
+                    ratiofloat= float(ratio.replace(',',''))
+                    margin_ratio.append(ratiofloat)
+ 
+            
+            kekka = numpy.corrcoef(stock_price,margin_ratio)[0]
+
+            stock_name = stock_name.find("h2")
+            stock_name = stock_name.text
+            messagebox.showinfo('結果',stock_name+'\n\n相関係数は'+str(kekka))
 
 lbl = tkinter.Label(text='株価コード')
 lbl.place(x=40, y=70)
@@ -62,5 +77,3 @@ btn.place(x=300, y=300)
 
 
 tki.mainloop()
-
-soukankeisu()
